7f92d6a20c1f9aec525643674611309be2c3c8f1
Guido Amodio <guido.amodio@gmail.com>
2020-07-13 18:18:14 +0200

    inserimento posizione lista ordinata

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 // array esistente
-var cognomi= ["abc", "def", "ghi","lmn",];
+var cognomi= ["abc", "def", "ghi","lmn"];
 
 // inserimento cognome da parte dell'utente
-cognomi.push(prompt("inserisci un cognome"));
+var inserito = prompt("inserisci un cognome");
+cognomi.push(inserito);
 
 
 // iniziamo a rielaborare la lista partendo dalle maiuscole e dalle minuscole
@@ -13,10 +14,8 @@ var cognomiCap= []; //array di destinazione
 
 // mi creo un ciclo for che va a prendere tutti i cognomi dal primo all'ultimo, li scompone e li reinserisce nell'array
 for (var i = 0; i < cognomi.length; i++) {
-    cognomiPrima= cognomi[i].charAt(0);
-    cognomiPrima = cognomiPrima.toUpperCase();
-    cognomiResto= cognomi[i].slice(1);
-    cognomiResto= cognomiResto.toLowerCase();
+    cognomiPrima = cognomi[i].charAt(0).toUpperCase();
+    cognomiResto = cognomi[i].slice(1).toLowerCase();
     var cogPrimaResto = cognomiPrima + cognomiResto;
 
     cognomiCap.push(cogPrimaResto);
@@ -26,12 +25,36 @@ cognomiCap.sort();
 
 // provo a stamparla sulla pagina in una lista ordinata
 
-var cognomiOl;
+var cognomiOl="<li>" + cognomiCap[0] + "</li>";
 
-for (var j = 0; j < cognomiCap.length; j++) {
-    cognomiOl+= "<li>" + cognomiCap[j] + "</li>";
+for (var j = 1; j < cognomiCap.length; j++) {
+    cognomiOl += "<li>" + cognomiCap[j] + "</li>";
 }
 
-document.getElementById('surname-list').innerHTML=(cognomiOl);
+document.getElementById('surname-list').innerHTML=cognomiOl;
 
-console.log(cognomiCap);
+
+var insTemp = inserito.slice(1).toLowerCase();
+inserito = inserito.charAt(0).toUpperCase();
+inserito = inserito + insTemp;
+
+// console.log(insTemp);
+console.log(inserito);
+
+
+
+
+// for (var k = 0; k < cognomiCap.length; k++) {
+//     if (cognomiCap[k] == inserito) {
+//         console.log(k+1);
+//     }
+// }
+
+var k=0;
+while(cognomiCap[k] != inserito) {
+    k++;
+}
+
+k+=1;
+
+document.getElementById('position').innerHTML=("il numero da te inserito si trova nella posizione numero " + k);
